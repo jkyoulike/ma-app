3a6ae3d2555d5a46c8cfa8225df25a03811537cd
jkyoulike <1533096867@qq.com>
2019-11-04 14:50:13 +0800

    第一次更新

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="app">
+    <div class="header">
+      <div class="header-left"><span class="iconfont">&#xe610;</span></div>
+      <div class="header-input">
+        <input type="text">
+      </div>
+    </div>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+body{
+  margin: 0;
+  padding: 0;
+}
+.header{
+  height: 3rem;
+  background:rgb(88, 208, 245);
+}
+.header-left{
+  float: left;
+}
+.header-left .iconfont{
+  font-size: 24px;
+  fill: aliceblue;
+  color: aliceblue;
+  margin-left: 1rem;
+  margin-right: 1rem;
+}
+.header-input input{
+  height: 2rem;
+  width: 10rem;
+}
+</style>
