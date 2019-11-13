9dc92b70155c6902768f7af6e51409c9f3c6f3a8
jkyoulike <1533096867@qq.com>
2019-11-13 16:31:08 +0800

    123

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,5 @@
 <template>
-  <div id="app">
-    <div class="header">
-      <div class="header-left">
-          <div class="iconfont">&#xe610;</div>
-      </div>
-      <div class="header-input">
-        <span class="iconfont">&#xe647;</span> 输入城市/景点/游玩主题
-      </div>
-      <div class="header-righe">城市
-      <span class="iconfont">&#xe736;</span>
-      </div>
-    </div>
     <router-view/>
-  </div>
 </template>
 
 <script>
@@ -22,35 +9,4 @@ export default {
 </script>
 
 <style>
-body{
-  margin: 0;
-  padding: 0;
-}
-.header{
-  background:#00afc7;
-  line-height:4rem;
-  display:flex;
-}
-.header-left{
-  float: left;
-  width:2rem;
-  color:#fff;
-  margin-left:1rem;
-}
-.header-input{
-  height:2rem;
-  line-height:2rem;
-  color:#ccc;
-  flex:1;
-  background:#fff;
-  margin-top:1rem;
-  border-radius:5px;
-}
-.header-righe{
-  margin:0rem 1rem 0rem 1rem;
-  color:#fff;
-  font-weight:bold;
-  font-family:"宋体";
-}
-
 </style>
--- src/main.js
+++ src/main.js
@@ -5,8 +5,12 @@ import App from './App'
 import router from './router'
 import './assets/iconfont/iconfont.css'
 
+import fastClick from 'fastclick'
+import VueAwesomeSwiper from 'vue-awesome-swiper'
+import 'swiper/dist/css/swiper.css'
+Vue.use(VueAwesomeSwiper)
+fastClick.attach(document.body)
 Vue.config.productionTip = false
-
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
