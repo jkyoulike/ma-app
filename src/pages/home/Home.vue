57c1a95103f5d0be95fb67cc2dd9dbaa29a2e2ae
jkyoulike <1533096867@qq.com>
2019-11-27 16:08:44 +0800

    hot

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -3,6 +3,7 @@
     <home-header></home-header>
     <home-swiper></home-swiper>
     <home-icons></home-icons>
+    <home-hot></home-hot> 
 </div>
 </template>
 
@@ -10,12 +11,14 @@
 import HomeHeader from './components/header'
 import HomeSwiper from './components/swiper'
 import HomeIcons from './components/Icons'
+import HomeHot from './components/hot'
 export default {
   name: 'Home',
   components:{
       HomeHeader,
       HomeSwiper,
       HomeIcons,
+      HomeHot
   }
 }
 </script>
